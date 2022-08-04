ac62ab25921dfe17772fb77c0529dd9db3115c49
sanchay-1109 <79998603+sanchay-1109@users.noreply.github.com>
2022-08-04 09:28:24 +0530

    Commit

--- expenseWebsite/expenses/urls.py
+++ expenseWebsite/expenses/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
 
-urlpatterns={
-    path('', views.index, name='expenses),
-}
+ path('', views.index, name="expenses"),
+    path('add-expense', views.add_expense, name="add-expenses")
