6d6ae43f1715866900a135835378176303d62dd3
sanchay-1109 <79998603+sanchay-1109@users.noreply.github.com>
2022-08-05 23:14:21 +0530

    Commit

--- expenseWebsite/expenses/views.py
+++ expenseWebsite/expenses/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'expenses/index.html')
+    return render(request, 'expenseWebsite/expenseWebsite/templates/expenses/index.html')
 
 def add_expense(request):
-    return render(request, 'expenses/add_expense.html')
+    return render(request, 'expenseWebsite/expenseWebsite/templates/expenses/add_expense.html')
