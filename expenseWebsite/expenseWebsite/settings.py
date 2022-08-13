c039a3c6e4a6849730583917d4e653d7f49ef3d8
sanchay-1109 <79998603+sanchay-1109@users.noreply.github.com>
2022-08-13 09:09:09 +0530

    Commit

--- expenseWebsite/expenseWebsite/settings.py
+++ expenseWebsite/expenseWebsite/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR=[os.path.join(BASE_DIR,'expenseWebsite/expenseWebsite/static')]
+STATICFILES_DIRS=[(os.path.join(BASE_DIR,'expensewebsite/static'))]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
