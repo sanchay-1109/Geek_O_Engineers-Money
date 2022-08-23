00aecb4d5ed5268747b6ef8ee1249836e04e047f
sanchay-1109 <79998603+sanchay-1109@users.noreply.github.com>
2022-08-23 15:16:58 +0530

    Commit

--- expenseWebsite/authentication/views.py
+++ expenseWebsite/authentication/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import JsonResponse
+import json
+
 # Create your views here.
+
+
+class userNameValidationView(View):
+    def post(self,request):
+        data=json.loads(request.body)
+        username=data['username']
+        if not str(username).isalnum():
+            return JsonResponse({'username_error':'username should contaion only alphanumeric Characters'})
 class RegistrationView(View):
     def get(self, request):
         return render(request, 'authentication/register.html')
